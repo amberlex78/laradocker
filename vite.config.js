46938e5d9d385200a4dd4e44eaa520ffead28188
amberlex78 <amberlex78@gmail.com>
2026-09-16 22:19:17 +0300

    fix(vite): configure HMR client port

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
         strictPort: true,
         hmr: {
             host: process.env.VITE_HMR_HOST ?? 'localhost',
-            port: Number(process.env.VITE_HMR_PORT ?? process.env.VITE_PORT ?? 5173),
+            clientPort: Number(process.env.VITE_HMR_PORT ?? process.env.VITE_PORT ?? 5173),
         },
         watch: {
             ignored: ['**/storage/framework/views/**'],
